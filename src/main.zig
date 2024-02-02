00382cb85ca030abc90e18fb23ebb9adaf0eda44
Froxcey <danichen204@gmail.com>
2024-02-02 17:09:55 +0800

    Add endsWith and startsWith

--- src/main.zig
+++ src/main.zig
@@ -6,47 +6,64 @@ pub usingnamespace Sushi;
 const Sushi = struct {
     data: ArrayList,
 
-    pub fn from(alloc: std.mem.Allocator, data: []const u8) !Sushi {
+    pub fn from(alloc: std.mem.Allocator, initial_str: ?[]const u8) !Sushi {
         var string = Sushi{
             .data = ArrayList.init(alloc),
         };
-        try string.data.appendSlice(data);
+        if (initial_str != null)
+            try string.data.appendSlice(initial_str.?);
         return string;
     }
     pub fn free(self: Sushi) void {
         self.data.deinit();
     }
-    pub fn copy(self: Sushi) !Sushi {
+    pub fn clone(self: Sushi) !Sushi {
         return self.from(self.data.allocator, self.data.items);
     }
+    //pub fn clone_str() []const u8 {}
 
     pub fn append(self: *Sushi, data: []const u8) !void {
         try self.data.appendSlice(data);
     }
-    pub fn eql(self: Sushi, otherString: []const u8) bool {
-        return std.mem.eql(u8, self.data.items, otherString);
+    pub fn endsWith(self: Sushi, search_str: []const u8) bool {
+        if (self.data.items.len < search_str.len) return false;
+        for (search_str, 0..) |char, i| {
+            if (self.data.items[self.data.items.len - search_str.len + i] != char) return false;
+        }
+        return true;
     }
-    pub fn indexOf(self: Sushi, searchString: []const u8) ?usize {
-        const selfStr = self.data.items;
-        pos: for (0..selfStr.len - searchString.len + 1) |curr_i| {
-            for (searchString, 0..) |check_char, check_i| {
+    pub fn eql(self: Sushi, compare_str: []const u8) bool {
+        return std.mem.eql(u8, self.to_str(), compare_str);
+    }
+    pub fn indexOf(self: Sushi, search_str: []const u8) ?usize {
+        const selfStr = self.to_str();
+        pos: for (0..selfStr.len - search_str.len + 1) |curr_i| {
+            for (search_str, 0..) |check_char, check_i| {
                 if (check_char != selfStr[curr_i + check_i]) continue :pos;
             }
             return curr_i;
         }
         return null;
     }
-    pub fn includes(self: Sushi, searchString: []const u8) bool {
-        const pos = self.indexOf(searchString);
+    pub fn includes(self: Sushi, search_str: []const u8) bool {
+        const pos = self.indexOf(search_str);
         return pos != null;
     }
-    //pub fn replace(self: Str, searchValue: []const u8, replaceValue: []const u8) void {
-    //    _ = replaceValue;
-    //    _ = searchValue;
-    //    _ = self;
-    //    return;
-    //}
-    pub fn str(self: Sushi) []const u8 {
+    //pub fn lastIndexOf(self: Sushi, search_str: [] const u8) ?usize {}
+    //pub fn match(self: Sushi, search_regex: []const u8) []const u8 {}
+    //pub fn padEnd(self: *Sushi, target_length: usize, pad_string: ?[]const u8) !void {}
+    //pub fn padStart(self: *Sushi, target_length: usize, pad_string: ?[]const u8) !void {
+    //pub fn repeat(self: *Sushi, count: usize) {}
+    //pub fn replace(self: Str, searchValue: []const u8, replaceValue: []const u8) void {}
+    //pub fn split(self: Sushi, separator: []cconst u8) {}
+    pub fn startsWith(self: Sushi, search_str: []const u8) bool {
+        if (self.data.items.len < search_str.len) return false;
+        for (search_str, 0..) |char, i| {
+            if (self.data.items[i] != char) return false;
+        }
+        return true;
+    }
+    pub fn to_str(self: Sushi) []const u8 {
         return self.data.items;
     }
     pub fn toLowerCase(self: *Sushi) void {
@@ -61,23 +78,26 @@ const Sushi = struct {
             self.data.items[i] = char - 32;
         }
     }
-    //pub fn trim(self: Str) void {
-    //    _ = self;
-    //    return;
-    //}
+    //pub fn trim(self: Str) void {}
+    //pub fn trimEnd(self: Str) void {}
+    //pub fn trimStart(self: Str) void {}
 };
 
 test "Core Test" {
     const alloc = std.testing.allocator;
     var str = try Sushi.from(alloc, "Chiissu!!");
     defer str.free();
+    const expect = std.testing.expect;
 
     try str.append("Yes");
-    try std.testing.expect(str.indexOf("!!").? == 7);
-    try std.testing.expect(str.includes("!!"));
-    try std.testing.expect(!str.includes("BS"));
+    try expect(str.indexOf("!!").? == 7);
+    try expect(str.includes("!!"));
+    try expect(!str.includes("BS"));
     str.toLowerCase();
-    try std.testing.expect(str.eql("chiissu!!yes"));
+    try expect(str.eql("chiissu!!yes"));
     str.toUpperCase();
-    try std.testing.expect(str.eql("CHIISSU!!YES"));
+    try expect(str.startsWith("CHIIS"));
+    try expect(!str.startsWith("Fyb"));
+    try expect(str.endsWith("YES"));
+    try expect(!str.endsWith("NO"));
 }
